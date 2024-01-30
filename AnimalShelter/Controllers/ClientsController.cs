494839d441f38e5a840737b736a1d1f7a8a312f2
Brianca Knight <briancaknight@gmail.com>
2024-01-30 13:13:44 -0800

    add edit view and route to clients
    Co-authored-by: Aaron Mejia <aaron@mejia.dev>

--- AnimalShelter/Controllers/ClientsController.cs
+++ AnimalShelter/Controllers/ClientsController.cs
@@ -40,5 +40,20 @@ namespace AnimalShelter.Controllers
       return View(selectedClient);
     }
     
+    public ActionResult Edit (int id)
+    {
+      Client selectedClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(selectedClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Clients.Update(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+
   }
 }
